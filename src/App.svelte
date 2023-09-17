838ef0e7a4be2f20549174d5e6efc8647db5be8a
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2023-09-17 21:27:42 +0200

    Add first api endpoint

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,16 @@
   import Navbar from "./lib/ui/Navbar.svelte";
   import ThemeProvider from "./ThemeProvider.svelte";
   import Footer from "./lib/ui/Footer.svelte";
+
+  onMount(async () => {
+    const body = JSON.stringify({
+      name: "Erik",
+    });
+    const response = await (
+      await fetch("/api/trips", { method: "POST", body: body })
+    ).text();
+    console.log(response);
+  });
 </script>
 
 <ThemeProvider>
