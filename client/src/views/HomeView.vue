bce52b074fe14028bddd1bb00bdad4e4324b84d8
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2024-04-26 00:14:59 +0200

    Add new trip page and move checklist in there. Add trip cards in the home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="container max-w-screen-md mx-auto py-20 shadow-m">
     <section class="py-12">
-      <h2 class="text-6xl font-extrabold text-[#08384e] max-w-prose">Lag og del pakkelister</h2>
-      <p class="my-4 text-lg max-w-prose">
-        Campster gjør det enkelt å samarbeide på felles pakkelister!
+      <h2 class="text-6xl font-bold text-[#08384e] max-w-prose">Planlegg tur og del pakkelister</h2>
+      <p class="my-4 text-lg font-semibold max-w-prose">
+        Campster gjør det enkelt å planlegge felles turer og pakkelister!
       </p>
     </section>
 
-    <CheckList />
+    <section>
+      <h2 class="text-3xl font-semibold text-[#08384e] py-5">Nylige turer</h2>
+      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+        <a href="#">
+          <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Overnatting på Rustfjellhei
+          </h5>
+        </a>
+
+        <div class="flex flex-row space-x-2 py-0">
+          <user-group-icon />
+          <p class="mb-3 font-normal text-gray-400 dark:text-gray-400">Deltakere: 5 personer</p>
+        </div>
+        <div class="flex flex-row space-x-2 py-0">
+          <map-pin-alt-icon />
+          <p class="mb-3 font-normal text-gray-400 dark:text-gray-400">Deltakere: 5 personer</p>
+        </div>
+        <router-link
+          to="/trip"
+          class="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-blue-300"
+        >
+          Gå til side
+          <arrow-right-icon class="text-white ms-2" />
+        </router-link>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import CheckList from '../components/CheckList.vue'
+import ArrowRightIcon from '../components/icons/ArrowRightIcon.vue'
+import UserGroupIcon from '../components/icons/UserGroupIcon.vue'
+import MapPinAltIcon from '../components/icons/MapPinAltIcon.vue'
 
 export default {
   setup() {
     return {}
   },
-  components: { CheckList }
+  components: { UserGroupIcon, ArrowRightIcon, MapPinAltIcon }
 }
 </script>
---
+++ client/src/views/TripView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container max-w-screen-md mx-auto py-20">
+    <section class="py-12">
+      <h2 class="text-5xl font-bold text-[#08384e] max-w-prose">Tur til Rustfjellhei</h2>
+    </section>
+
+    <div
+      class="text-sm font-medium text-center bg-white p-4 rounded-lg shadow text-gray-500 border-b border-gray-200"
+    >
+      <ul class="flex flex-wrap -mb-px">
+        <li class="me-2">
+          <a
+            href="#"
+            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+            >Diskusjon</a
+          >
+        </li>
+        <li class="me-2">
+          <a
+            href="#"
+            class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active"
+            aria-current="page"
+            >Pakkelister</a
+          >
+        </li>
+        <li class="me-2">
+          <a
+            href="#"
+            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+            >Innstillinger</a
+          >
+        </li>
+        <li class="me-2">
+          <a
+            href="#"
+            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+            >Deltakere</a
+          >
+        </li>
+      </ul>
+
+      <check-list class="my-12" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CheckList from '@/components/CheckList.vue'
+export default {
+  components: { CheckList }
+}
+</script>
+
+<style></style>
