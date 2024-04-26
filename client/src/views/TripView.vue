40cbdaf9f187c07b5838858f7245a40c89582fdd
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2024-04-26 16:35:17 +0200

    🎨 Restructure TripView inside a <main> tag in favor of header section and div

--- client/src/views/TripView.vue
+++ client/src/views/TripView.vue
@@ -1,46 +1,44 @@
 <template>
   <div class="container max-w-screen-md mx-auto py-20">
-    <section class="py-12">
-      <h2 class="text-5xl font-bold text-[#08384e] max-w-prose">Tur til Rustfjellhei</h2>
-    </section>
+    <main class="bg-white p-5 rounded-lg shadow">
+      <h2 class="text-5xl font-bold text-[#08384e] max-w-prose py-12">Tur til Rustfjellhei</h2>
 
-    <div
-      class="text-sm font-medium text-center bg-white p-4 rounded-lg shadow text-gray-500 border-b border-gray-200"
-    >
-      <ul class="flex flex-wrap -mb-px">
-        <li class="me-2">
-          <a
-            href="#"
-            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
-            >Diskusjon</a
-          >
-        </li>
-        <li class="me-2">
-          <a
-            href="#"
-            class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active"
-            aria-current="page"
-            >Pakkelister</a
-          >
-        </li>
-        <li class="me-2">
-          <a
-            href="#"
-            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
-            >Innstillinger</a
-          >
-        </li>
-        <li class="me-2">
-          <a
-            href="#"
-            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
-            >Deltakere</a
-          >
-        </li>
-      </ul>
+      <nav class="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+        <ul class="flex flex-wrap -mb-px">
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+              >Diskusjon</a
+            >
+          </li>
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active"
+              aria-current="page"
+              >Pakkelister</a
+            >
+          </li>
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+              >Innstillinger</a
+            >
+          </li>
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+              >Deltakere</a
+            >
+          </li>
+        </ul>
+      </nav>
 
       <check-list class="my-12" />
-    </div>
+    </main>
   </div>
 </template>
 
