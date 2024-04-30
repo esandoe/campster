5bbeb84606835f198a6ccf609dd24e75f4c8c1f0
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2024-04-30 23:08:02 +0200

    🐛 Fix bug when updating quantity using native number input
    When the number input was changed using the native buttons or by writing the number manually, the state was not change. This change adds the "v-model" instead of "v-bind" which updates the checklist items quantity when the inputs value changes.

--- client/src/views/TripCheckListView.vue
+++ client/src/views/TripCheckListView.vue
@@ -33,7 +33,7 @@
                   id="first_product"
                   class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1"
                   placeholder="0"
-                  v-bind:value="item.quantity"
+                  v-model="item.quantity"
                   required
                 />
               </div>
