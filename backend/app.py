9f95e1f1890929ff9c1694df597bbf0b089434a8
putetrekk <17160920+putetrekk@users.noreply.github.com>
2024-05-01 20:14:05 +0200

    🗃 Add database to backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,107 @@
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Column, ForeignKey, Table
+from sample_data import sample_trip_item_data, sample_list_entry_data, sample_trip_data
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+trip_participants = Table(
+    "trip_participants",
+    db.Model.metadata,
+    Column("trip_id", ForeignKey("trip.id"), primary_key=True),
+    Column("user_id", ForeignKey("user.id"), primary_key=True),
+)
+
+
+@dataclass
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+
+@dataclass
+class Item(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    trip_id: Mapped[int] = mapped_column(ForeignKey("trip.id"))
+    name: Mapped[str] = mapped_column(unique=True)
+    shared_target: Mapped[int] = mapped_column(default=0)
+    created_at: Mapped[datetime]
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+
+@dataclass
+class ListEntry(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    trip_id: Mapped[int] = mapped_column(ForeignKey("trip.id"))
+    amount: Mapped[int]
+    shared_amount: Mapped[int]
+    packed: Mapped[bool]
+    created_at: Mapped[datetime]
+    updated_at: Mapped[datetime]
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+
+@dataclass
+class Trip(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    participants: Mapped[list[User]] = relationship(secondary=trip_participants)
+    items: Mapped[list[Item]] = relationship(Item)
+    listEntries: Mapped[list[ListEntry]] = relationship(ListEntry)
+
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///campster.db"
+db.init_app(app)
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    for trip in sample_trip_data:
+        new_trip = Trip(name=trip["name"])
+        trip_participants = [User(id=user_id) for user_id in trip["participants"]]
+        db.session.add(new_trip)
+    for item in sample_trip_item_data:
+        trip_item = Item(
+            name=item["name"],
+            trip_id=1,
+            shared_target=item.get("shared_target", 0),
+            created_at=datetime.now(),
+        )
+        db.session.add(trip_item)
+    db.session.commit()
+
+    for entry in sample_list_entry_data:
+        list_entry = ListEntry(
+            item_id=entry["item_id"],
+            user_id=entry["user_id"],
+            trip_id=entry["trip_id"],
+            amount=entry["amount"],
+            shared_amount=entry["shared_amount"],
+            packed=entry["packed"],
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+        )
+        db.session.add(list_entry)
+    db.session.commit()
 
 # enable CORS
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -15,24 +113,23 @@ def ping_pong():
     return jsonify("pong!")
 
 
-@app.route("/api/trip", methods=["GET"])
-def get_trip():
-    items = [
-        { "product": '12 personers glamping lavvo', "quantity": 1, "packed": False },
-        { "product": 'Grytesett, 2L + 1L gryter, 2x tallerken', "quantity": 1, "packed": False},
-        { "product": 'Zenbivy sovesystem', "quantity": 1, "packed": False},
-        { "product": 'Retro Campingvogn', "quantity": 1, "packed": False},
-        { "product": 'Fargerik Hengekøye', "quantity": 1, "packed": False},
-        { "product": 'Bærbar Grill', "quantity": 1, "packed": False},
-        { "product": 'Utendørs Projektor og Lerret', "quantity": 1, "packed": False},
-        { "product": 'Stjernetittende Teleskop', "quantity": 1, "packed": False},
-        { "product": 'Sammenleggbar Campingstol', "quantity": 1, "packed": False},
-        { "product": 'Varmt Kakaosett', "quantity": 1, "packed": False},
-        { "product": 'Vandrestaver', "quantity": 1, "packed": False},
-        { "product": 'Vanntett Kart og Kompass', "quantity": 1, "packed": False},
-        { "product": 'Fotovennlig Kamera', "quantity": 1, "packed": False }
-    ]
+@app.route("/api/trips", methods=["GET"])
+def get_trips():
+    trips = Trip.query.all()
+    return jsonify(trips)
+
+
+@app.route("/api/trip/<trip_id>/items", methods=["GET"])
+def get_trip_items(trip_id):
+    items = Trip.query.filter_by(id=trip_id).first().items
     return jsonify(items)
+
+
+@app.route("/api/trip/<trip_id>/user/<user_id>/entries", methods=["GET"])
+def get_trip_list_entries(trip_id, user_id):
+    entries = ListEntry.query.filter_by(trip_id=trip_id, user_id=user_id).all()
+    return jsonify(entries)
+
 
 if __name__ == "__main__":
     app.run()
---
+++ backend/sample_data.py
@@ -0,0 +1,51 @@
+sample_trip_item_data = [
+    {"name": "12 personers glamping lavvo", "quantity": 1, "packed": False},
+    {
+        "name": "Grytesett, 2L + 1L gryter, 2x tallerken",
+        "quantity": 1,
+        "packed": False,
+    },
+    {"name": "Zenbivy sovesystem", "quantity": 1, "packed": False},
+    {"name": "Retro Campingvogn", "quantity": 1, "packed": False},
+    {"name": "Fargerik Hengekøye", "quantity": 1, "packed": False},
+    {"name": "Bærbar Grill", "quantity": 1, "packed": False},
+    {"name": "Utendørs Projektor og Lerret", "quantity": 1, "packed": False},
+    {"name": "Stjernetittende Teleskop", "quantity": 1, "packed": False},
+    {"name": "Sammenleggbar Campingstol", "quantity": 1, "packed": False},
+    {"name": "Varmt Kakaosett", "quantity": 1, "packed": False},
+    {"name": "Vandrestaver", "quantity": 1, "packed": False},
+    {"name": "Vanntett Kart og Kompass", "quantity": 1, "packed": False},
+    {"name": "Fotovennlig Kamera", "quantity": 1, "packed": False},
+]
+
+sample_list_entry_data = [
+    {
+        "trip_id": 1,
+        "item_id": 1,
+        "amount": 1,
+        "shared_amount": 1,
+        "group_target": 0,
+        "packed": False,
+        "user_id": 1,
+    },
+    {
+        "trip_id": 1,
+        "item_id": 2,
+        "amount": 1,
+        "shared_amount": 1,
+        "group_target": 0,
+        "packed": False,
+        "user_id": 1,
+    },
+    {
+        "trip_id": 1,
+        "item_id": 3,
+        "amount": 1,
+        "shared_amount": 1,
+        "group_target": 0,
+        "packed": False,
+        "user_id": 1,
+    },
+]
+
+sample_trip_data = [{"name": "Glamping i Lofoten", "participants": [1, 3, 4]}]
