0d8ac924fddfffba4efb071993b3d488235d0f9c
putetrekk <17160920+putetrekk@users.noreply.github.com>
2024-05-02 21:50:43 +0200

    ✨ Add supply target endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from database import db, Trip, TripParticipant, ParticipantItem, User
+from database import db, Trip, TripParticipant, ParticipantItem, User, SupplyTarget
 
 from sample_data import sample_trips
 
@@ -35,8 +35,14 @@ def get_trip_participants(trip_id):
     return jsonify(participants)
 
 
+@app.route("/api/trip/<trip_id>/supply-targets", methods=["GET"])
+def get_supply_targets(trip_id):
+    targets = SupplyTarget.query.filter_by(trip_id=trip_id).all()
+    return jsonify(targets)
+
+
 @app.route("/api/trip/<trip_id>/participant/<participant_id>/items", methods=["GET"])
-def get_participants_items(trip_id, participant_id):
+def get_participant_items(trip_id, participant_id):
     items = (
         TripParticipant.query.filter_by(trip_id=trip_id, id=participant_id)
         .first_or_404()
@@ -44,12 +50,6 @@ def get_participants_items(trip_id, participant_id):
     )
 
     return jsonify(items)
-
-
-# @app.route("/api/trip/<trip_id>/user/<user_id>/entries", methods=["GET"])
-# def get_trip_list_entries(trip_id, user_id):
-#     entries = ListEntry.query.filter_by(trip_id=trip_id, user_id=user_id).all()
-#     return jsonify(entries)
 
 
 if __name__ == "__main__":
