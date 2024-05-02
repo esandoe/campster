c3965cdb2c3de2e089b61f05a79e54730aebc07b
putetrekk <17160920+putetrekk@users.noreply.github.com>
2024-05-02 20:39:36 +0200

    ♻️ Extract database to separate file

--- backend/app.py
+++ backend/app.py
@@ -1,76 +1,12 @@
-from dataclasses import dataclass
-from datetime import datetime
 from flask import Flask, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey
+from database import db, Trip, TripParticipant, ParticipantItem, User
+
 from sample_data import (
     sample_trip_item_data,
     sample_trips,
     sample_users,
 )
-
-
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
-
-
-@dataclass
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(unique=True)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now)
-
-
-@dataclass
-class ParticipantItem(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    participant_id: Mapped[int] = mapped_column(ForeignKey("trip_participant.id"))
-    supply_target: Mapped["SupplyTarget"] = relationship("SupplyTarget")
-    supply_target_id: Mapped[int] = mapped_column(
-        ForeignKey("supply_target.id"), nullable=True
-    )
-    name: Mapped[str] = mapped_column(unique=True)
-    quantity: Mapped[int] = mapped_column(default=0)
-    packed: Mapped[bool] = mapped_column(default=False)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now)
-
-
-@dataclass
-class TripParticipant(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user: Mapped[User] = relationship(User)
-    trip_id: Mapped[int] = mapped_column(ForeignKey("trip.id"))
-    items: Mapped[list[ParticipantItem]] = relationship("ParticipantItem")
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now)
-
-
-@dataclass
-class Trip(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    participants: Mapped[list[TripParticipant]] = relationship(TripParticipant)
-    name: Mapped[str] = mapped_column(unique=True)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now)
-
-
-@dataclass
-class SupplyTarget(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    trip: Mapped[Trip] = relationship(Trip)
-    trip_id: Mapped[int] = mapped_column(ForeignKey("trip.id"))
-    name: Mapped[str] = mapped_column(unique=True)
-    target_quantity: Mapped[int] = mapped_column(default=0)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
 
 # instantiate the app
