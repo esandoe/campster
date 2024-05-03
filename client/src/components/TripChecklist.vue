52229a44a38d0b5457cdaa08fb8e473a79cca1f9
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2024-05-03 17:30:08 +0200

    💄 Switch toggles for checkboxes

--- client/src/components/TripChecklist.vue
+++ client/src/components/TripChecklist.vue
@@ -58,10 +58,12 @@
           </td>
           <td class="px-1 md:px-6 py-3 whitespace-nowrap">
             <label class="inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" class="sr-only peer" v-bind:checked="item.packed" />
-              <div
-                class="relative w-11 h-6 bg-gray-300 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"
-              ></div>
+              <input
+                type="checkbox"
+                value=""
+                class="w-5 h-5 text-xl text-blue-600 bg-white border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+                v-bind:checked="item.packed"
+              />
             </label>
           </td>
           <td class="px-1 md:px-6 py-3 whitespace-nowrap">
@@ -161,7 +163,6 @@ function editItemName(item, newName) {
 
 onMounted(async () => {
   try {
-    
     const params = useRoute().params
 
     const response = await fetch(`/api/trip/${params.tripId}/participant/${params.listId}/items`)
