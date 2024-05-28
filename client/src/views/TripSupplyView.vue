4a8e00c2199d72ac86aa59ebefec445b80c495eb
putetrekk <17160920+putetrekk@users.noreply.github.com>
2024-05-28 23:31:10 +0200

    ✨ Add delete button for supply targets

--- client/src/views/TripSupplyView.vue
+++ client/src/views/TripSupplyView.vue
@@ -9,21 +9,32 @@
               <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                 {{ supplyTarget.name }}
               </p>
-              <p class="text-sm text-gray-500 truncate dark:text-gray-400 divide-y divide-none">
+              <div class="text-sm text-gray-500 truncate dark:text-gray-400 divide-y divide-none">
                 <div v-for="item in supplyTarget.items" :key="item.id" class="px-2">
                   {{ item.name }}: {{ item.quantity }}
                 </div>
-              </p>
+              </div>
             </div>
             <div class="inline-flex items-center text-base text-gray-900 dark:text-white">
               Mål: <span class="font-semibold mx-4"> {{ supplyTarget.target_quantity }}</span>
             </div>
-            
+
             <div class="inline-flex items-center text-base text-gray-900 dark:text-white">
               Bidrag: <span class="font-semibold mx-4"> {{ supplyTarget.sum }}</span>
             </div>
-            <div class="inline-flex space-x-3 w-1/4 items-center text-base text-gray-900 dark:text-white">
-              <div></div> <ProgressBar :progress="supplyTarget.progress"/></div>
+            <div
+              class="inline-flex space-x-3 w-1/4 items-center text-base text-gray-900 dark:text-white"
+            >
+              <ProgressBar :progress="supplyTarget.progress" />
+            </div>
+            <div class="inline-flex items-center text-base text-gray-900 dark:text-white">
+              <button
+                @click="removeSupplyTarget(supplyTarget)"
+                class="font-medium text-red-600 hover:underline"
+              >
+                Fjern
+              </button>
+            </div>
           </div>
         </li>
       </div>
@@ -32,24 +43,38 @@
 </template>
 
 <script setup>
-import ProgressBar from '@/components/ProgressBar.vue';
-import {  onMounted, ref } from 'vue'
+import ProgressBar from '@/components/ProgressBar.vue'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const supplyList = ref(null)
+const tripId = useRoute().params.tripId
+
+async function removeSupplyTarget(supplyTarget) {
+  const response = await fetch(`/api/trip/${tripId}/supply-targets/${supplyTarget.id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id: supplyTarget.id })
+  })
+
+  if (response.ok) {
+    supplyList.value = supplyList.value.filter((supply) => supply.id !== supplyTarget.id)
+  }
+}
 
 onMounted(async () => {
   try {
-    const tripId = useRoute().params.tripId
     const response = await fetch(`/api/trip/${tripId}/supply-targets`)
 
     const supplyListResponse = await response.json()
-    supplyList.value = supplyListResponse.map(supply => {
-      const sum = supply.items.map(i => i.quantity).reduce((partialSum, q) => partialSum + q, 0)
+    supplyList.value = supplyListResponse.map((supply) => {
+      const sum = supply.items.map((i) => i.quantity).reduce((partialSum, q) => partialSum + q, 0)
       return {
         ...supply,
-        "sum": sum,
-        "progress": (sum/supply.target_quantity * 100).toFixed(2)
+        sum: sum,
+        progress: ((sum / supply.target_quantity) * 100).toFixed(2)
       }
     })
   } catch (err) {
