2721329835e3adcbeb4702d27a8eee8ce0006413
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2024-05-30 20:32:38 +0200

    ✨ Make hamburger menu work!

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -6,32 +6,18 @@
         <span class="self-center text-2xl font-semibold whitespace-nowrap">Campster</span>
       </a>
       <button
-        data-collapse-toggle="navbar-default"
         type="button"
         class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
         aria-controls="navbar-default"
         aria-expanded="false"
+        @click="showMenu = !showMenu"
       >
         <span class="sr-only">Open main menu</span>
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
+        <HamburgerMenuIcon />
       </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div class="w-full md:block md:w-auto" :class="{ hidden: !showMenu }">
         <ul
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0"
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 bg-[#ffebdd] md:bg-transparent rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0"
         >
           <li v-if="user">
             <span class="py-0 px-2 flex items-center text-white">
@@ -76,8 +62,10 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
+import HamburgerMenuIcon from './icons/HamburgerMenuIcon.vue'
 
 const user = ref(null)
+const showMenu = ref(false)
 
 onMounted(async () => {
   const response = await fetch('/api/profile')
