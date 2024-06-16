d773d3f502e942c59fc5925e308d717faa00a36b
putetrekk <17160920+putetrekk@users.noreply.github.com>
2024-06-16 15:43:43 +0200

    Use comment style for post editor

--- client/src/views/TripHomeView.vue
+++ client/src/views/TripHomeView.vue
@@ -61,15 +61,21 @@
 
     <h2 class="py-5 text-lg font-semibold text-gray-900">Innlegg</h2>
     <form v-if="isParticipant" @submit.prevent="createAttachment">
-      <div class="flex flex-col items-left">
-        <textarea v-model="attachment.text"
-          class="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm text-gray-900"
-          placeholder="Skriv et innlegg..." />
-        <input type="file" v-on:change="uploadFile" />
-        <button type="submit"
-          class="inline-flex items-center justify-center px-3 py-2 mt-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300">
-          Publiser
-        </button>
+      <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+          <label for="text" class="sr-only">Skriv innlegg</label>
+          <textarea v-model="attachment.text" id="text" name="text" rows="3"
+            class="w-full px-3 py-2 border-none resize-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm text-gray-900"
+            placeholder="Skriv innlegg"></textarea>
+            <button type="submit"
+              class="inline-flex items-center justify-center px-3 py-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300">
+              Publiser
+            </button>
+          <input type="file"
+            v-on:change="uploadFile"
+            class="inline-flex items-center justify-center px-3 py-2 pl-6">
+          </input>
+        </div>
       </div>
     </form>
     <div v-for="attachment in sortedAttachments" :key="attachment.id">
