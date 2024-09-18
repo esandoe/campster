30ad6dc7a00949f9c47fbd83f30241275ea60aa3
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2024-09-18 22:20:25 +0200

    💄 Improve Checklist usability on smaller screens (#34)
    Multiple changes done to improve how the checklist table is shown on smaller screens. Also added a "edit" mode, which lets us show a more clean table when not making changes.

--- client/src/components/TripChecklist.vue
+++ client/src/components/TripChecklist.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+  <div class="relative overflow-x-auto">
+    <SecondaryButton @click="editMode = !editMode" class="m-2">
+      {{ editMode ? 'Lagre' : 'Rediger' }}
+    </SecondaryButton>
+
     <p v-if="!items">
       <ListSkeleton />
     </p>
-    <table v-else class="table-fixed text-sm text-left text-gray-500 rounded-md">
-      <thead class="text-lg text-gray-700 bg-gray-200">
+
+    <table v-else class="table-fixed text-left text-gray-500 rounded-md">
+      <thead class="text-xs uppercase text-gray-700 bg-gray-50">
         <tr>
-          <th class="px-2 py-3 w-full" colspan="2">Product</th>
-          <th class="px-1 md:px-6 py-3 whitespace-nowrap">SupplyTarget</th>
-          <th class="px-1 md:px-6 py-3 whitespace-nowrap">Qty</th>
-          <th class="px-2 md:px-6 py-3 whitespace-nowrap">Packed</th>
-          <th class="px-1 md:px-6 py-3 whitespace-nowrap">Action</th>
+          <th v-if="editMode"></th>
+          <th class="px-2 py-2 w-full">Produkt</th>
+          <th class="px-1 md:px-6 py-2 max-md:hidden">Forsyningsmål</th>
+          <th class="px-1 md:px-6 py-2 whitespace-nowrap">Antall</th>
+          <th v-if="!editMode" class="px-1 md:px-6 py-2 whitespace-nowrap">Pakket</th>
+          <th v-if="editMode">Slett</th>
         </tr>
       </thead>
       <TransitionGroup name="checklist" tag="tbody">
         <tr v-if="items.length === 0">
-          <td colspan="5" class="text-left w-full px-5 py-3">
+          <td colspan="5" class="text-left w-full px-5 py-2">
             <p class="text-md">
               Du har ingenting i pakkelisten din enda! Vil du kopiere listen din fra sist?
             </p>
@@ -28,96 +34,109 @@
         <tr
           v-for="(item, pos) in items"
           :key="item.id"
-          class="border-b checklist-item"
+          class="checklist-item"
           :class="{
-            'cursor-move bg-gray-200': dragHoverPosition === pos,
-            'bg-gray-100': dragHoverPosition !== pos
+            'border-b-4 border-b-gray-600': dragHoverPosition === pos && dragStartPosition < pos,
+            'border-t-4 border-t-gray-600': dragHoverPosition === pos && dragStartPosition > pos,
+            'border-b border-b-gray-300': dragHoverPosition !== pos
           }"
           @dragstart="(e) => dragstart_handler(e, pos)"
           @drop="(e) => dropHandler(e, pos)"
           @dragenter.prevent="dragHoverPosition = pos"
           @dragover.prevent
         >
-          <td draggable="true" class="cursor-pointer pr-0">
+          <td v-if="editMode" draggable="true" class="cursor-pointer pr-0">
             <DraggableItemIcon />
           </td>
-          <td class="w-full px-0 py-1 text-lg text-gray-900">
-            <input
-              class="w-full block px-4 py-2 rounded-md outline-none"
-              :class="{
-                'bg-transparent hover:bg-white cursor-pointer': !item._status?.editing,
-                'bg-white': item._status?.editing
-              }"
-              :value="item.name"
-              @click="item._status.editing = true"
-              @focus="item._status.editing = true"
-              @keydown.enter="(event) => editItemName(item, event.target.value)"
-              @blur="(event) => editItemName(item, event.target.value)"
-            />
-          </td>
-          <td class="px-1 md:px-6 py-3 whitespace-nowrap">
-            <select
-              class="block w-full px-4 py-2 text-sm border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
-              :value="item.supply_target_id"
-              @input="(event) => updateItem(item, 'supply_target_id', event.target.value)"
-            >
-              <option value="" selected>---</option>
-              <option
-                v-for="supplyTarget in supplyTargets"
-                :key="supplyTarget.id"
-                :value="supplyTarget.id"
-                @input="editItemSupplyTarget(item, supplyTarget.id)"
-              >
-                {{ supplyTarget.name }}
-              </option>
-            </select>
-          </td>
-          <td class="px-1 md:px-6 py-3 whitespace-nowrap">
-            <div class="flex items-center">
-              <SecondaryButton
-                class="me-3 !p-1 !rounded-full"
-                @click="updateItem(item, 'quantity', --item.quantity)"
-              >
-                <span class="sr-only">Quantity button</span>
-                <MinusIcon class="text-gray-500 h-3 w-3" />
-              </SecondaryButton>
+          <td class="w-full px-2 py-2 text-gray-900">
+            <div>
+              <input
+                class="w-full py-0 md:py-1 rounded-sm outline-none"
+                :class="{
+                  'bg-transparent hover:bg-white cursor-pointer': !item._status?.editing,
+                  'bg-gray-50 outline-1 outline-gray-400': item._status?.editing
+                }"
+                :value="item.name"
+                @click="item._status.editing = true"
+                @focus="item._status.editing = true"
+                @keydown.enter="(event) => editItemName(item, event.target.value)"
+                @blur="(event) => editItemName(item, event.target.value)"
+              />
+            </div>
 
-              <div>
-                <input
-                  type="number"
-                  id="first_product"
-                  class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1"
-                  placeholder="0"
-                  :value="item.quantity"
-                  @input="(event) => updateItem(item, 'quantity', event.target.value)"
-                  required
-                />
+            <div class="md:hidden">
+              <div class="px-0 py-0 pb-1" v-if="item.supply_target && !editMode">
+                <label class="text-xs"
+                  ><span class="md:hidden">Forsyningsmål: </span
+                  >{{ item.supply_target?.name }}</label
+                >
               </div>
-              <SecondaryButton
-                class="ms-3 !p-1 !rounded-full"
-                @click="updateItem(item, 'quantity', ++item.quantity)"
-              >
-                <span class="sr-only">Quantity button</span>
-                <PlusIcon class="text-gray-500 h-3 w-3" />
-              </SecondaryButton>
+              <div class="px-0 py-0" v-if="editMode">
+                <select
+                  class="text-xs text-blue-600 py-1.5 border-0 appearance-none focus:outline-none focus:ring-0 focus:border-1 focus:border-gray-200"
+                  :value="item.supply_target_id"
+                  @input="(event) => updateItem(item, 'supply_target_id', event.target.value)"
+                >
+                  <option value="" selected>---</option>
+                  <option
+                    v-for="supplyTarget in supplyTargets"
+                    :key="supplyTarget.id"
+                    :value="supplyTarget.id"
+                    @input="editItemSupplyTarget(item, supplyTarget.id)"
+                  >
+                    {{ supplyTarget.name }}
+                  </option>
+                </select>
+              </div>
             </div>
           </td>
-          <td class="px-1 md:px-6 py-3 whitespace-nowrap">
+          <td class="max-md:hidden px-0 md:px-6 py-2 whitespace-nowrap">
+            <div class="px-0 py-0" v-if="item.supply_target && !editMode">
+              <label class="text-sm"
+                ><span class="md:hidden">Forsyningsmål:</span>{{ item.supply_target?.name }}</label
+              >
+            </div>
+            <div class="px-0 py-0" v-if="editMode">
+              <select
+                class="text-sm text-gray-500 bg-transparent border-0 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200"
+                :value="item.supply_target_id"
+                @input="(event) => updateItem(item, 'supply_target_id', event.target.value)"
+              >
+                <option value="" selected>---</option>
+                <option
+                  v-for="supplyTarget in supplyTargets"
+                  :key="supplyTarget.id"
+                  :value="supplyTarget.id"
+                  @input="editItemSupplyTarget(item, supplyTarget.id)"
+                >
+                  {{ supplyTarget.name }}
+                </option>
+              </select>
+            </div>
+          </td>
+          <td class="px-1 md:px-6 py-2 whitespace-nowrap">
+            <CounterInput
+              :value="item.quantity"
+              @update:value="($event) => updateItem(item, 'quantity', $event)"
+              :editable="editMode"
+            />
+          </td>
+          <td v-if="!editMode" class="px-1 md:px-6 py-2 whitespace-nowrap">
             <CheckBox
               v-model="item.packed"
               @input="updateItem(item, 'packed', !item.packed)"
             ></CheckBox>
           </td>
-          <td class="px-1 md:px-6 py-3 whitespace-nowrap">
-            <button @click="removeItem(item)" class="font-medium text-red-600 hover:underline">
-              Fjern
+          <td v-if="editMode" class="px-1 md:px-6 py-2 whitespace-nowrap text-right">
+            <button @click="removeItem(item)" class="hover:underline">
+              <CloseIcon class="bg-red w-10 h-10"></CloseIcon>
             </button>
           </td>
         </tr>
       </TransitionGroup>
     </table>
 
-    <div class="w-full px-6 py-4 font-semibold bg-gray-50 text-gray-400" ref="addNewRef">
+    <div class="w-full px-6 py-4 font-semibold" ref="addNewRef">
       <div class="relative">
         <TextInput
           class="!p-4 mb-3"
@@ -142,7 +161,6 @@
 <script setup>
 import ListSkeleton from '@/components/ListSkeleton.vue'
 import DraggableItemIcon from '@/components/icons/DraggableItemIcon.vue'
-import MinusIcon from '@/components/icons/MinusIcon.vue'
 import PlusIcon from '@/components/icons/PlusIcon.vue'
 import { scrollIntoView } from '@/components/utils'
 import { onMounted, ref } from 'vue'
@@ -151,6 +169,8 @@ import PrimaryButton from '@/components/ui/PrimaryButton.vue'
 import SecondaryButton from './ui/SecondaryButton.vue'
 import CheckBox from './ui/CheckBox.vue'
 import TextInput from './ui/TextInput.vue'
+import CloseIcon from './icons/CloseIcon.vue'
+import CounterInput from './ui/CounterInput.vue'
 
 const items = ref(null)
 const supplyTargets = ref(null)
@@ -158,7 +178,10 @@ const newItemName = ref('')
 const addNewRef = ref(null)
 const errorMsg = ref(null)
 const dragHoverPosition = ref(null)
+const dragStartPosition = ref(null)
 const remainingSuggestions = ref(null)
+
+const editMode = ref(false)
 
 const params = useRoute().params
 
@@ -172,6 +195,7 @@ Array.prototype.move = function (from, to) {
 }
 
 function dragstart_handler(event, startPos) {
+  dragStartPosition.value = startPos
   event.dataTransfer.setData('startPos', startPos)
   const parentRow = event.target.parentElement
   event.dataTransfer.setDragImage(parentRow, parentRow.width, parentRow.height)
@@ -321,7 +345,9 @@ onMounted(async () => {
 .checklist-item {
   transition:
     all 0.5s ease,
-    background-color 0s;
+    background-color 0s,
+    border-bottom 0.2s,
+    border-top 0.2s;
 }
 
 .checklist-enter,
