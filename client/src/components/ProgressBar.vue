471e79c25fcfb7085a2b8b95189a03ef43bf4b58
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2025-06-24 21:56:39 +0200

    Juster threshold da teksten hopper fra inni progressbar til utenfor til 40% og sett teksten til venstrejustert når ledende kant på progressbar når utenfor.

--- client/src/components/ProgressBar.vue
+++ client/src/components/ProgressBar.vue
@@ -2,16 +2,17 @@
   <div class="w-full bg-gray-200 rounded-lg relative h-6">
     <div class="bg-blue-600 h-6 rounded-lg transition-all duration-200" :style="barWidth">
       <div
-        v-if="progressNum >= 50"
+        v-if="progressNum >= 40"
         class="absolute h-6 flex items-center text-xs font-medium text-white text-right"
-        :style="progressTextStyle"
+        :style="progressBarTextStyle"
       >
         <span v-if="label">{{ label }}&nbsp;/&nbsp;</span>{{ progress }}%
       </div>
     </div>
     <div
-      v-if="progressNum < 50"
-      class="absolute inset-0 flex items-center justify-center text-xs font-medium text-black"
+      v-if="progressNum < 40"
+      class="absolute inset-0 flex items-center text-xs font-medium text-black"
+      :style="progressRightStyle"
     >
       <span v-if="label">{{ label }}&nbsp;/&nbsp;</span>{{ progress }}%
     </div>
@@ -28,11 +29,17 @@ const props = defineProps({
 
 const barWidth = computed(() => `width: ${props.progress > 100 ? 100 : props.progress}%`)
 const progressNum = computed(() => Number(props.progress))
-const progressTextStyle = computed(() => ({
+const progressBarTextStyle = computed(() => ({
   left: 0,
   width: `${props.progress > 100 ? 100 : props.progress}%`,
   justifyContent: 'center',
   position: 'absolute',
   pointerEvents: 'none'
+}))
+const progressRightStyle = computed(() => ({
+  left: `${props.progress > 100 ? 100 : props.progress}%`,
+  paddingLeft: '0.5rem',
+  justifyContent: 'left',
+  position: 'absolute'
 }))
 </script>
